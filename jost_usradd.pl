8f7c664aa28e32d65bc77d6925af5da45f0c6f39
darkstar76 <pablo.moreira@jost.com.ar>
2011-07-24 20:02:56 -0300

    modified:   jost.conf 	modified:   jost_usradd.pl 	deleted:    lib/JostFile.pm 	deleted:    lib/Obj.pm 	deleted:    lib/Vmail.pm 	deleted:    trash/DBI- 	deleted:    trash/j.ods 	deleted:    trash/jo.fkt 	deleted:    trash/jost.ods 	deleted:    trash/jost_dom.pl 	deleted:    trash/jost_usr.pl 	deleted:    trash/lib/Bool.pm 	deleted:    trash/lib/Domain.pm 	deleted:    trash/lib/Domain.pm~ 	deleted:    trash/lib/FileSystem.pm 	deleted:    trash/lib/MyUtil.pm 	deleted:    trash/lib/Param.pm 	deleted:    trash/lib/Param.pm~ 	deleted:    trash/lib/Sql.pm 	deleted:    trash/lib/User.pm 	deleted:    trash/lib/Util.pm~ 	deleted:    trash/nss.tar
    	bin/

--- jost_usradd.pl
+++ jost_usradd.pl
@@ -5,33 +5,60 @@ use User;
 use strict;
 use warnings;
 use MyUtil;
-use JostFile;
+use Mkdir;
 
 sub main{
 	
 	my %param = MyUtil::MyOp(@ARGV);
 	
 	my $user = new User;
+	my $dominio = new Domain;
 	#Envia la la referencia al objeto y la referencia al hash de parametros
 	#Debuelve usuario Seteado.
 	
 	MyUtil::Set($user,\%param); 
 	
 	
+	$dominio->Set_group_name($user->Get_domain());
 	
-	if ($user->Control())
+	if(!$dominio->Existe())
 	{
-		$user->Default();
-		my $cmd = $user->Get_cmd();
-		$user->$cmd();
-		my $dir = new JostFile();
-		$dir->mkhomedir($user);
-		
-		print "\n";
-	}				
+		print $dominio->Get_error(),"\n";
+		return 0;
+	}
 	
-	exit;
+	
+	#Control parametros
+	if(!$user->Control())
+	{
+		print $user->Get_error(),"\n";
+		return 0;
+	}
+	$user->Default();
+	
+	if($user->Existe())
+	{
+		print "Usuario Existe\n";
+		return 0;
+	}
+	
+	$user->add();
+	
+	my $dir = new Mkdir();
+	$dir->Set_user($user);
+	
+	if(!$dir->existe_homedir())
+	{
+		print "No existe home para el usuario\n";
+		
+		if(!$dir->existe_parenthomedir())
+		{
+			$dir->mkparenthomedir();
+		}
+		$dir->mkhomedir();
+	}
 	
 }
 
 main();
+print "Exit\n";
